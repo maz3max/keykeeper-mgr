d16a591f183e46aea74a96a1874f5360f345d3d0
Maximilian Deubel <maximilian.deubel@gmail.com>
2020-11-05 13:56:44 +0100

    merge key_db into syn_central

--- sync_central.py
+++ sync_central.py
@@ -7,8 +7,28 @@ import serial.serialutil
 import os
 import sys
 import time
-from key_db import KeykeeperDB
 from enum import IntEnum
+
+
+class KeykeeperDB:
+    def __init__(self):
+        self.coins = {}
+        self.identity = []
+        self.load()
+
+    def load(self, coins="coins.txt", central="central.txt"):
+        coin_list = []
+        identity = None
+        with open(coins, "r") as f:
+            for line in f:
+                m = re.match(r"(.{17})\s+(.{32})\s+(.{32})\s+(.{64})", line)
+                if m:
+                    self.coins[m.group(1)] = m.groups()[1:]
+        with open(central, "r") as f:
+            line = f.readline()
+            m = re.match(r"(.{17})\s+(.{32})", line)
+            if m:
+                self.identity = m.groups()
 
 
 class StatusType(IntEnum):
